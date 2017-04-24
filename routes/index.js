00549bbe5935c0817772340d71c18eefbd0f1ed6
tim5046 <tim5046@gmail.com>
2017-04-24 13:36:04 -0700

    secrets list

--- routes/index.js
+++ routes/index.js
@@ -19,26 +19,46 @@ var h = helpers.registered;
 
 router.get("/", loggedInOnly, (req, res) => {
   Secret.find({
-    where: { author: req.user._id }
+    author: req.user._id
   })
-    .then(secretList => {
-      res.render("index", secretList);
-    })
-    .catch(e => res.status(500).send(e.stack));
+  .populate('viewers')
+  .then(secrets => {
+    res.render("index", {secrets});
+  })
+  .catch(e => res.status(500).send(e.stack));
 });
 
 router.post("/secret", loggedInOnly, (req, res) => {
   let secret = req.body.secret;
-  console.log(secret);
   Secret.create({
     author: req.user._id,
     body: secret,
     viewers: [req.user._id]
   })
-    .then(() => {
-      res.redirect(h.indexPath());
-    })
-    .catch(e => res.status(500).send(e.stack));
+  .then((secret) => {
+    res.redirect(h.indexPath());
+  })
+  .catch(e => res.status(500).send(e.stack));
+});
+
+router.get("/secrets", loggedInOnly, (req, res) => {
+  let accessibleSecrets;
+  Secret.find({},
+    {_id:0, body:1, viewers:{$elemMatch:{ $eq: req.user._id}}}
+  )
+  .populate('viewers')
+  .then(accessbile => {
+    console.log("accessbile secrets", accessbile)
+    accessibleSecrets = accessbile;
+    return Secret.find({},
+      {_id:0, body:1, viewers:{$elemMatch:{ $ne: req.user._id}}}
+    )
+  })
+  .then((unavailableSecrets) => {
+    console.log("unavailableSecrets: ", unavailableSecrets)
+    res.render("secrets/index", {accessibleSecrets, unavailableSecrets});
+  })
+  .catch(e => res.status(500).send(e.stack));
 });
 
 module.exports = router;
