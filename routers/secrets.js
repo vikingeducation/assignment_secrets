af5f5158a81cce79ac08e5b4f1442684f547b969
Will Timpson <will.timpson@gmail.com>
2017-08-14 18:24:03 -0400

    getting there

--- routers/secrets.js
+++ routers/secrets.js
@@ -4,49 +4,70 @@ const h = require("../helpers");
 const { loggedInOnly } = require("../services/Session");
 
 router.get("/", loggedInOnly, async (req, res) => {
-  const authored = await Secret.find({ author: req.user._id });
-  // console.log(authored);
-  res.render("secrets/index", { authored });
+  try {
+    const authored = await Secret.find({ author: req.user._id });
+    res.render("secrets/index", { authored });
+  } catch (e) {
+    res.status(500).end(e.stack);
+  }
 });
 
 router.post("/", loggedInOnly, async (req, res) => {
-  const body = req.body.secret;
-  const author = req.user._id;
-  await Secret.create({ author, body });
-  res.redirect(h.secretsPath());
-});
-
-router.get("/:id", loggedInOnly, async (req, res) => {
-  const _id = req.params.id;
-  await Secret.findOne({ _id });
+  try {
+    const body = req.body.secret;
+    const author = req.user._id;
+    await Secret.create({ author, body });
+    res.redirect(h.secretsPath());
+  } catch (e) {
+    res.status(500).end(e.stack);
+  }
 });
 
 router.get("/all", loggedInOnly, async (req, res) => {
-  const shared = req.user.sharedSecrets.map(secret => secret._id);
+  try {
+    const shared = req.user.sharedSecrets.map(secret => secret._id);
 
-  let secrets = await Secret.find().populate("author");
-  secrets = secrets.map(secret => {
-    let mappedSecret = {
-      id: secret._id,
-      author: secret.author,
-      body: null,
-      requested: false
-    };
-    if (shared.includes(secret._id) || secret.author._id.equals(req.user._id)) {
-      mappedSecret.body = secret.body;
-    }
-
-    for (let request of secret.requests) {
-      if (request.equals(req.user._id)) {
-        mappedSecret.requested = true;
+    let secrets = await Secret.find().populate("author");
+    secrets = secrets.map(secret => {
+      let mappedSecret = {
+        id: secret._id,
+        author: secret.author,
+        body: null,
+        requested: false
+      };
+      if (
+        shared.includes(secret._id) ||
+        secret.author._id.equals(req.user._id)
+      ) {
+        mappedSecret.body = secret.body;
       }
-    }
-    return mappedSecret;
-  });
 
-  // console.log(secrets);
+      for (let request of secret.requests) {
+        if (request.equals(req.user._id)) {
+          mappedSecret.requested = true;
+        }
+      }
+      return mappedSecret;
+    });
 
-  res.render("secrets/all", { secrets });
+    res.render("secrets/all", { secrets });
+  } catch (e) {
+    res.status(500).end(e.stack);
+  }
 });
 
+router.get("/:id", loggedInOnly, async (req, res) => {
+  try {
+    // let secret = await Secret.findById(req.params.id);
+    // secret.requests.push(req.user._id);
+    // await secret.save();
+    await Secret.update(
+      { _id: req.params.id },
+      { $push: { requests: req.user._id } }
+    );
+    res.redirect("back");
+  } catch (e) {
+    res.status(500).end(e.stack);
+  }
+});
 module.exports = router;
