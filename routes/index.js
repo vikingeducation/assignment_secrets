863a78e368fb32754fc3bea32a23e2482caf6a9e
Eric Glover <etgt28@mail.missouri.edu>
2017-08-14 17:58:34 -0500

    show index of secrets

--- routes/index.js
+++ routes/index.js
@@ -5,51 +5,62 @@ const RequestPermission = require("../models").RequestPermission;
 const { loggedInOnly } = require("../services/session");
 
 router.get("/", loggedInOnly, async (req, res) => {
-	//get all the secrets
-	let user = req.user;
-	let pendingSecrets = {};
-	let secrets = await Secret.findAll({
-		where: { authorid: user.id }
-	});
+  //get all the secrets
+  try {
+    let user = req.user;
+    let pendingSecrets = {};
+    let secrets = await Secret.findAll({
+      where: { authorid: user.id }
+    });
 
-	let result = await RequestPermission.findAll({
-		where: { secretId: secrets[0].id }
-	});
+    let result = await RequestPermission.findAll({
+      where: { secretId: secrets[0].id }
+    });
 
-	// secrets.forEach((secret, i) => {
-	// 	RequestPermission.findAll({
-	// 		where: { secretId: secret.id }
-	// 	}).then(requests => {
-	// 		pendingSecrets[i] = requests;
-	// 	});
-	// });
+    // secrets.forEach((secret, i) => {
+    // 	RequestPermission.findAll({
+    // 		where: { secretId: secret.id }
+    // 	}).then(requests => {
+    // 		pendingSecrets[i] = requests;
+    // 	});
+    // });
 
-	// console.log(pendingSecrets, "??");
+    // console.log(pendingSecrets, "??");
 
-	return res.render("index", { secrets });
+    return res.render("index", { secrets });
+  } catch (e) {
+    console.log(`error = `, e.stack);
+  }
 });
 
 router.get("/secrets", loggedInOnly, async (req, res) => {
-	try {
-		let secrets = await Secret.findAll({
-			where: { $ne: { authorid: req.user.id } }
-		});
+  try {
+    console.log(`req.user.id = `, req.user.id);
+    let secrets = await Secret.findAll({
+      where: { authorid: { $ne: req.user.id } }
+    });
 
-		res.render("secrets", { secrets });
-	} catch (e) {
-		console.error(e.stack);
-	}
+    let newSecrets = secrets.map(async secret => {
+      let author = await User.findById(secret.authorid);
+      return (secret["author"] = author);
+      // let author = await User.findById(secret.authorid);
+    });
+
+    res.render("secrets", { secrets });
+  } catch (e) {
+    console.error(e.stack);
+  }
 });
 
 router.post("/secrets", loggedInOnly, async (req, res) => {
-	let user = req.user;
+  let user = req.user;
 
-	let secret = await Secret.create({
-		secret: req.body.secret,
-		authorid: user.id
-	});
+  let secret = await Secret.create({
+    secret: req.body.secret,
+    authorid: user.id
+  });
 
-	res.redirect("/");
+  res.redirect("/");
 });
 
 module.exports = router;
