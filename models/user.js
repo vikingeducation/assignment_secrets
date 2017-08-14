331b59ed5047a199aca27f9b97a16f2287657b81
Stephanie Barker <song_mage@yahoo.com>
2017-08-14 13:37:01 -0400

    working on seeding

--- models/user.js
+++ models/user.js
@@ -1,16 +1,19 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt");
 
 const UserSchema = new Schema(
 	{
 		username: String,
 		passwordHash: String,
-		secrets: [
-			{
+		secrets: [{
+			author: Boolean,
+			secret: {
 				type: Schema.Types.ObjectId,
 				ref: "Secret"
 			}
-		]
+		}]
+		
 		// orderItems: [
 		// 	{
 		// 		type: Schema.Types.ObjectId,
@@ -22,6 +25,14 @@ const UserSchema = new Schema(
 		timestamps: true
 	}
 );
+
+// personSchema.virtual('fullName').get(function () {
+//   return this.name.first + ' ' + this.name.last;
+// });
+
+UserSchema.virtual("password").set(function(password) {
+	this.passwordHash = bcrypt.hash(password, 10)
+});
 
 // Create the model with a defined schema
 const User = mongoose.model("User", UserSchema);
--- seeds/index.js
+++ seeds/index.js
@@ -1,19 +1,24 @@
 const mongoose = require("mongoose");
 const { Secret, User } = require("../models");
+const bcrypt = require("bcrypt");
 
 const seeds = () => {
 
+  let users = []
   for(let i = 0; i < 10, i++) {
     let newUser = new User({
-      username:
-      passwordHash:
-      secrets:
+      username: `user${i}`
+      passwordHash: `password${i}`
     })
+
+    users.push(newUser);
   }
 
   for(let i = 0; i <20; i++) {
     let newSecret = new Secret({
-    
+      body: `I have ${i} cats!`
+      author: users[i % 10]
+      users: [users[(i + 1) % 10], users[(i + 2) % 10], users[(i + 3) % 10]]
     })
   }
 }
