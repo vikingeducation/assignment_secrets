29b6eaf3bdc8a5a86c9629db6ffe0c4e2ec12233
EdTriplett <EdTriplett@users.noreply.github.com>
2017-08-14 19:11:50 -0400

    My & Shared secrets

--- routes/secrets.js
+++ routes/secrets.js
@@ -16,14 +16,28 @@ const {
 
 //home
 router.get("/", loggedInOnly, (req, res) => {
+  let theUser;
+  let mySecrets;
+  let sharedSecrets;
   let [username, junk] = req.cookies.sessionId.split(":");
   console.log(`username: ${username}`);
   User.findOne({ username: username})
   .then((user) => {
+    theUser = user
     return Secret.find({ owner: user._id })
   })
+  .then(secrets=>{
+    mySecrets = secrets;
+    return Secret.find({
+      owner: {$ne: theUser._id},
+      approvedViewers: theUser._id
+      
+    })
+  })
   .then((secrets) => {
-    return res.render("home", { secrets });
+    sharedSecrets = secrets;
+ //   console.log()
+    return res.render("home", { mySecrets, sharedSecrets });
   })
   .catch(e => res.status(500).send(e.stack));
 });
