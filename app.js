34217603070a44de5f069e9b78382af2d39802eb
Ian Halverson <halversoncode@gmail.com>
2017-08-14 13:15:51 -0400

    doing seeds

--- app.js
+++ app.js
@@ -1,3 +1,56 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const expressSession = require("express-session");
+const cookieParser = require("cookie-parser");
+const methodOverride = require("method-override");
+const getPostSupport = require("express-method-override-get-post-support");
+const expressHandlebars = require("express-handlebars");
+const morgan = require("morgan");
+const morganToolKit = require("morgan-toolkit")(morgan);
+const hbs = expressHandlebars.create({
+  partialsDir: "views/partials",
+  defaultLayout: "application"
+});
+const mongoose = require("mongoose");
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(`${__dirname}/public`));
+app.use(morgan("tiny"));
+app.use(morganToolKit());
+app.use(methodOverride(getPostSupport.callback, getPostSupport.options));
+app.use(
+  expressSession({
+    secret: "stephanieiscool",
+    resave: false,
+    saveUninitialized: true
+  })
+);
+
+app.use((req, res, next) => {
+  if (mongoose.connection.readyState) {
+    next();
+  } else {
+    require("./mongo")().then(() => next());
+  }
+});
+
+//routes here
+app.get("/", (req, res) => {
+  res.send("yay");
+});
+
+const port = process.env.PORT || process.argv[2] || 3000;
+const host = "localhost";
+let args;
+process.env.NODE_ENV === "production" ? (args = [port]) : (args = [port, host]);
+args.push(() => {
+  console.log(`Listening: http://${host}:${port}`);
+});
+app.listen.apply(app, args);
+
+module.exports = app;
---
+++ seeds/index.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const { Secret, User } = require("../models");
+
+const seeds = () => {
+
+  for(let i = 0; i < 10, i++) {
+    let newUser = new User({
+      username:
+      passwordHash:
+      secrets:
+    })
+  }
+
+  for(let i = 0; i <20; i++) {
+    let newSecret = new Secret({
+    
+    })
+  }
+}
